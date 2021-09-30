f5a9b5b2d42215b773385b9108de00722eb28d2e
pilauria <ptr.lauria@gmail.com>
2021-09-30 13:31:41 +0200

    finished 1 session

--- js_cardioSession/session1/index_start.js
+++ js_cardioSession/session1/index_start.js
@@ -1,3 +1,4 @@
+/////////////////////////////////////////////////
 // CHALLENGE 1: REVERSE A STRING
 // Return a string in reverse
 // ex. reverseString('hello') === 'olleh'
@@ -8,6 +9,7 @@ function reverseString(str) {
   return str.split('').reduce((revString, char) => char + revString, '');
 }
 
+////////////////////////////////////////////////
 // CHALLENGE 2: VALIDATE A PALINDROME
 // Return true if palindrome and false if not
 // ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
@@ -19,6 +21,7 @@ function isPalindrome(str) {
   return revString === lowReStr;
 }
 
+////////////////////////////////////////////////////
 // CHALLENGE 3: REVERSE AN INTEGER
 // Return an integer in reverse
 // ex. reverseInt(521) === 125
@@ -28,6 +31,7 @@ function reverseInt(int) {
   return revString * Math.sign(int);
 }
 
+//////////////////////////////////////////////////
 // CHALLENGE 4: CAPITALIZE LETTERS
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
@@ -68,7 +72,7 @@ function reverseInt(int) {
 //   }
 //   return strArr.join(' ');
 // }
-// /////////////////////////////
+
 // function capitalizeLetters(str) {
 //   return str
 //     .toLowerCase()
@@ -76,23 +80,86 @@ function reverseInt(int) {
 //     .map(word => word[0].toUpperCase() + word.substr(1))
 //     .join(' ');
 // }
-/////////////////////////////
-function capitalizeLetters(str) {
-  return str.replace(/\b[a-z]/gi, function (char) {
-    return char.toUpperCase();
-  });
-}
 
+// function capitalizeLetters(str) {
+//   return str.replace(/\b[a-z]/gi, function (char) {
+//     return char.toUpperCase();
+//   });
+// }
+
+////////////////////////////////////////////////////////
 // CHALLENGE 5: MAX CHARACTER
 // Return the character that is most common in a string
 // ex. maxCharacter('javascript') == 'a'
-function maxCharacter(str) {}
+// function maxCharacter(str) {
+//   // initialize an object to map the caracters in the string (how many times a character shows up)
+//   const charMap = {};
+//   let maxNum = 0;
+//   let maxChar = '';
+//   // transform the string in array and loop throuth it to populate the map object
+//   str.split('').forEach(char => {
+//     // if the character is already present sum it
+//     if (charMap[char]) {
+//       charMap[char]++;
+//       // otherwise start the count at 1
+//     } else {
+//       charMap[char] = 1;
+//     }
+//   });
+//   // console.log(charMap);
+//   // return the highest value (loop through the object)***
+//   for (let char in charMap) {
+//     // debugger;
+//     if (charMap[char] > maxNum) {
+//       maxNum = charMap[char];
+//       maxChar = char;
+//     }
+//   }
+//   return maxChar;
+// }
+
+// function maxCharacter(str) {
+//   const charMap = {};
+//   str.split('').forEach(char => {
+//     charMap[char] ? charMap[char]++ : (charMap[char] = 1);
+//   });
+//   // console.log(Object.keys(charMap));
+//   return Object.keys(charMap).reduce((prev, next) => {
+//     // console.log(`the prev is ${prev}: ${charMap[prev]}`);
+//     // console.log(next, charMap[next]);
+//     return charMap[prev] >= charMap[next] ? prev : next;
+//   });
+// }
 
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() {}
+// function fizzBuzz() {
+//   for (let i = 0; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log('FizzBuzz');
+//     } else if (i % 5 === 0) {
+//       console.log('buzz');
+//     } else if (i % 3 === 0) {
+//       console.log('fizz');
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// }
 
 // Call Function
-const output = capitalizeLetters('lA pIllOnola');
+const output = fizzBuzz();
 
 console.log(output);
+
+// // *** looping through objects ***
+// const object = { a: 1, b: 2, c: 3 };
+
+// for (const property in object) {
+//   console.log(`${property}: ${object[property]}`);
+// }
+
+// expected output:
+// "a: 1"
+// "b: 2"
+// "c: 3"

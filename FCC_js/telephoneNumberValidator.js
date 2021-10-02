dfd27f06e777607f71803c5053b9a6dcec6ace52
pilauria <ptr.lauria@gmail.com>
2021-10-02 10:57:11 +0200

    telephoneNumValidator

---
+++ FCC_js/telephoneNumberValidator.js
@@ -0,0 +1,24 @@
+// https://www.regexpal.com/
+// https://regex101.com/
+// validation of strings/ mach pattern on strings (REGEX)
+
+function telephoneCheck(str) {
+  const validPatterns = [
+    //555-555-5555
+    /^\d{3}-\d{3}-\d{4}$/,
+    //1 555-555-5555
+    /^1 \d{3}-\d{3}-\d{4}$/,
+    //1 (555) 555-5555
+    /^1 \(\d{3}\) \d{3}-\d{4}$/,
+    // 5555555555
+    /^\d{10}$/,
+    // (555)555-5555
+    /^\(\d{3}\)\d{3}-\d{4}$/,
+    // 1 555 555 5555
+    /^1 \d{3} \d{3} \d{4}$/,
+    // 1(555)555-5555
+    /1\(\d{3}\)\d{3}-\d{4}/,
+  ];
+
+  return validPatterns.some(pattern => pattern.test(str));
+}

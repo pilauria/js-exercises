71e80ed670efc73eaf2981be00b33fbeab581c31
pilauria <ptr.lauria@gmail.com>
2021-11-07 08:46:08 +0100

    r

--- test.js
+++ test.js
@@ -1,20 +1,43 @@
-function pickOdd(arr) {
-  const oddNumbers = []; // do not remove or change this line
+const directions = [
+  'Starting point: Ironhack Madrid',
+  '➔ Turn right toward P. de la Chopera',
+  '← At the roundabout, take the 1st exit onto P. de la Chopera',
+  '* Lune Creperie P. de la Chopera 33, Madrid',
+];
 
-  for (i = 0; i < arr.length; i++) {
-    if (i % 2 !== 0) {
-      oddNumbers.push(arr[i]);
-    }
-  }
+function obtainDirections(step) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      console.log(directions[step]);
 
-  return oddNumbers; // do not remove or change this line
+      if (!directions[step]) reject('Instructions not found.');
+      else resolve();
+    }, 2000);
+  });
 }
-console.log(pickOdd([2, 4, 5, 6, 7, 8, 9]));
 
-arr1 = [2, 4, 5, 6, 7];
+obtainDirections(0)
+  .then(() => obtainDirections(1))
+  .then(() => obtainDirections(2))
+  .then(() => obtainDirections(3))
+  .then(() => console.log('You arrived at your destination!'))
+  .catch(err => console.log(err));
 
-arr2 = [2];
+////////////////////////////////////////////////
+const urls = [
+  'https://jsonplaceholder.typicode.com/users',
+  'https://jsonplaceholder.typicode.com/posts',
+  'https://jsonplaceholder.typicode.com/albums',
+];
 
-console.log(arr1[2]);
-
-console.log(arr2.push('mamma'));
+Promise.all(
+  urls.map(url => {
+    return fetch(url).then(resp => resp.json());
+  })
+)
+  .then(results => {
+    console.log(results[0]);
+    console.log(results[1]);
+    console.log(results[2]);
+  })
+  .catch(() => console.log('error'));
